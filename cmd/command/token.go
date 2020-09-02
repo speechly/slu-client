407e4c529de57797b28adaa08fa7a2079fa88a10
Igor Suleymanov <igor@speechly.com>
2020-09-02 10:58:56 +0300

    Strip temporary token path of directories

--- cmd/command/token.go
+++ cmd/command/token.go
@@ -27,9 +27,19 @@ func removeCachedToken(cmd *cobra.Command, args []string) {
 }
 
 func getTokenPath() string {
+	log.Debugf("Using config path '%s' and identity URL '%s'", configFilePath, identityURL)
+
 	if configFilePath != "" || identityURL != "" {
+		// Use either config filename or cleaned identity url as prefix for temp config.
+		_, f := filepath.Split(configFilePath)
+		if f == "" {
+			_, f = filepath.Split(filepath.Clean(identityURL))
+		}
+
 		// Make sure we don't mix up tokens for different config files / custom identity URLs.
-		return filepath.Join(os.TempDir(), fmt.Sprintf("%s_%s", configFilePath, tokenFilename))
+		p := filepath.Join(os.TempDir(), fmt.Sprintf("%s_%s", f, tokenFilename))
+		log.Debugf("Using temporary token from '%s'", p)
+		return p
 	}
 
 	return filepath.Join(getConfigDir(), tokenFilename)
